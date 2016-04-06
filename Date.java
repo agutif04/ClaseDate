e3b981598b5bf48e2425c3ac0f36fb9b89b50b7d
e5agutif04 <e5agutif04@pc32.(none)>
2016-04-06 12:46:22 +0200

    no hemos hecho nada

--- Date.java
+++ Date.java
@@ -1,16 +1,44 @@
 class Date{
-    private int day;
+   
+    public Date(){
+    public int day;
     private int month;
     private int year;
+    }
+	public Date(int day;int month; int year)throws DateException()
+    	stringBuffer= new stringBuffer()
     boolean isSameYear(Date otherDate){
-	if(this.year==otherYear){
+	if(this.year==otherYear.getYear){
 	    return true;
 	}else{
 	    return false;
 	}
+	}
+    boolean isSameMonth(Date otherDate){
+	    if(this.month==otherMonth.getMonth){
+		return true;
+	    }else{
+		return false;
+	    }
+	    }
+    boolean isSameDay(Date otherDate){
+	if(this.Day==otherDay.getDay){
+	    return true;
+	}else {
+	    return false;
+	}
+    }
+	
 	int getDay(){
 	    return this.day;
 	}
 	void setDay(int day){
 	     this.day=day;
 	}
+	void setMonth(int month){
+		this.month=month;
+	    }
+	void setYear(int year){
+		this.year=year; 
+	    }
+}
